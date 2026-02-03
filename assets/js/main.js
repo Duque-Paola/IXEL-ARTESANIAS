f92b566dfc9b349f1ac21a1b4088f8e80b3ab9a8
AxlOlvera <axl.sanchezolvera@gmail.com>
2026-02-02 22:43:35 -0600

    Se agregó navbar funcional

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,15 @@
 import { createFooter } from './components/footer.js';
+import { createNavbar } from './components/navbar.js';
 
 // Inyectar el navbar en todas las páginas
+document.addEventListener('DOMContentLoaded', () => {
+  const navbarContainer = document.getElementById('navbar-container');
+  if (navbarContainer) {
+    navbarContainer.innerHTML = createNavbar();
+  }
+});
+
+// Inyectar el footer en todas las páginas
 document.addEventListener('DOMContentLoaded', () => {
   const footerContainer = document.getElementById('footer-container');
   if (footerContainer) {
